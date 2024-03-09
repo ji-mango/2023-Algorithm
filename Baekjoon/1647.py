3ef4f7906f552e20f3fe8b3554ef79e6d9e9f5a6
ji-mango <susie000301@naver.com>
2024-03-09 15:43:37 +0900

    Add: Baekjoon problem

--- Baekjoon/1647.py
+++ Baekjoon/1647.py
@@ -43,22 +43,50 @@
 
 
 # 24.02.21(복습~)
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
+#
+# N, M = map(int, input().split())
+# graph = []
+# for i in range(M):
+#     graph.append(list(map(int, input().split())))
+#
+# graph.sort(key = lambda x:x[2])
+#
+#
+# def find_parent(x):
+#     if parent[x] != x:
+#         parent[x] = find_parent(parent[x])
+#     return parent[x]
+#
+#
+# def union(a, b):
+#     a = find_parent(a)
+#     b = find_parent(b)
+#     if a < b:
+#         parent[b] = a
+#     else:
+#         parent[a] = b
+#
+#
+# parent = list(range(N+1))
+# answer = 0
+# last_edge = 0
+# for i in range(M):
+#     a, b, c = graph[i]
+#     if find_parent(a) == find_parent(b):
+#         continue
+#     union(a, b)
+#     answer += c
+#     last_edge = c
+#
+# print(answer - last_edge)
 
-N, M = map(int, input().split())
-graph = []
-for i in range(M):
-    graph.append(list(map(int, input().split())))
-
-graph.sort(key = lambda x:x[2])
-
-
+# 24.03.09 복습2~
 def find_parent(x):
     if parent[x] != x:
         parent[x] = find_parent(parent[x])
     return parent[x]
-
 
 def union(a, b):
     a = find_parent(a)
@@ -68,17 +96,21 @@ def union(a, b):
     else:
         parent[a] = b
 
+N, M = map(int, input().split())
+graph = []
+for i in range(M):
+    graph.append(list(map(int, input().split())))
 
 parent = list(range(N+1))
+
 answer = 0
-last_edge = 0
+last = 0
+graph.sort(key=lambda x:x[2])
 for i in range(M):
     a, b, c = graph[i]
-    if find_parent(a) == find_parent(b):
-        continue
-    union(a, b)
-    answer += c
-    last_edge = c
+    if find_parent(a) != find_parent(b):
+        answer += c
+        union(a, b)
+        last = c
 
-print(answer - last_edge)
-
+print(answer - last)
